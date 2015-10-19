cd9e72d7aaaec0744c50ce0cad93a9b5280353de
Marko Obrovac <marko.obrovac@gmail.com>
2015-10-19 21:40:07 +0200

    Update to service-template-node v0.2.2
    - Logging: log only whitelisted headers for privacy reasons
    - Routes: fully-asynchronous route loading
    - Bump dependencies' versions

--- app.js
+++ app.js
@@ -54,6 +54,17 @@ function initApp(options) {
         }
     }
 
+    // set up header whitelisting for logging
+    if(!app.conf.log_header_whitelist) {
+        app.conf.log_header_whitelist = [
+                'cache-control', 'content-type', 'content-length', 'if-match',
+                'user-agent', 'x-request-id'
+        ];
+    }
+    app.conf.log_header_whitelist = new RegExp('^(?:' + app.conf.log_header_whitelist.map(function(item) {
+        return item.trim();
+    }).join('|') + ')$', 'i');
+
     // set up the spec
     if(!app.conf.spec) {
         app.conf.spec = __dirname + '/spec.yaml';
@@ -100,15 +111,17 @@ function initApp(options) {
     // set the CORS and CSP headers
     app.all('*', function(req, res, next) {
         if(app.conf.cors !== false) {
-            res.header('Access-Control-Allow-Origin', app.conf.cors);
-            res.header('Access-Control-Allow-Headers', 'Accept, X-Requested-With, Content-Type');
+            res.header('access-control-allow-origin', app.conf.cors);
+            res.header('access-control-allow-headers', 'accept, x-requested-with, content-type');
+            res.header('access-control-expose-headers', 'etag');
         }
-        res.header('X-XSS-Protection', '1; mode=block');
-        res.header('X-Content-Type-Options', 'nosniff');
-        res.header('X-Frame-Options', 'SAMEORIGIN');
-        res.header('Content-Security-Policy', app.conf.csp);
-        res.header('X-Content-Security-Policy', app.conf.csp);
-        res.header('X-WebKit-CSP', app.conf.csp);
+        res.header('x-xss-protection', '1; mode=block');
+        res.header('x-content-type-options', 'nosniff');
+        res.header('x-frame-options', 'SAMEORIGIN');
+        res.header('content-security-policy', app.conf.csp);
+        res.header('x-content-security-policy', app.conf.csp);
+        res.header('x-webkit-csp', app.conf.csp);
+        sUtil.initAndLogRequest(req, app);
         next();
     });
 
@@ -164,29 +177,34 @@ function initProducer(app) {
 function loadRoutes (app) {
 
     // get the list of files in routes/
-    return fs.readdirAsync(__dirname + '/routes')
-    .map(function (fname) {
-        // ... and then load each route
-        // but only if it's a js file
-        if(!/\.js$/.test(fname)) {
-            return;
-        }
-        // import the route file
-        var route = require(__dirname + '/routes/' + fname);
-        route = route(app);
-        // check that the route exports the object we need
-        if(route.constructor !== Object || !route.path || !route.router || !(route.api_version || route.skip_domain)) {
-            throw new TypeError('routes/' + fname + ' does not export the correct object!');
-        }
-        // wrap the route handlers with Promise.try() blocks
-        sUtil.wrapRouteHandlers(route.router, app);
-        // determine the path prefix
-        var prefix = '';
-        if(!route.skip_domain) {
-            prefix = '/:domain/v' + route.api_version;
-        }
-        // all good, use that route
-        app.use(prefix + route.path, route.router);
+    return fs.readdirAsync(__dirname + '/routes').map(function(fname) {
+        return BBPromise.try(function() {
+            // ... and then load each route
+            // but only if it's a js file
+            if(!/\.js$/.test(fname)) {
+                return undefined;
+            }
+            // import the route file
+            var route = require(__dirname + '/routes/' + fname);
+            return route(app);
+        }).then(function(route) {
+            if(route === undefined) {
+                return undefined;
+            }
+            // check that the route exports the object we need
+            if(route.constructor !== Object || !route.path || !route.router || !(route.api_version || route.skip_domain)) {
+                throw new TypeError('routes/' + fname + ' does not export the correct object!');
+            }
+            // wrap the route handlers with Promise.try() blocks
+            sUtil.wrapRouteHandlers(route.router);
+            // determine the path prefix
+            var prefix = '';
+            if(!route.skip_domain) {
+                prefix = '/:domain/v' + route.api_version;
+            }
+            // all good, use that route
+            app.use(prefix + route.path, route.router);
+        });
     }).then(function () {
         // catch errors
         sUtil.setErrorHandler(app);
@@ -195,6 +213,7 @@ function loadRoutes (app) {
     });
 
 }
+
 
 /**
  * Creates and start the service's web server
@@ -220,6 +239,7 @@ function createServer(app) {
     });
 
 }
+
 
 /**
  * The service's entry point. It takes over the configuration
