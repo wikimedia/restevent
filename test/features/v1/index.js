6ea5e62bf6c960f0eabadd3133762649a0b0fb97
Marko Obrovac <mobrovac@wikimedia.org>
2015-10-30 20:03:12 +0100

    Schema routes: Add tests

--- test/features/v1/index.js
+++ test/features/v1/index.js
@@ -10,14 +10,29 @@ var assert = require('../../utils/assert.js');
 var server = require('../../utils/server.js');
 
 
-describe('stub', function() {
-
-    this.timeout(20000);
+describe('Topic definitions', function() {
 
     before(function () { return server.start(); });
 
-    it('should implement an actual test', function() {
+    it('get topics list', function() {
+        return preq.get({
+            uri: server.config.uri + 'wwww/v1/topics/'
+        }).then(function(res) {
+            assert.status(res, 200);
+            assert.deepEqual(!!res.body.items, true, 'No items returned!');
+        });
+    });
 
+    it('get topic schema', function() {
+        return preq.get({
+            uri: server.config.uri + 'wwww/v1/topics/example'
+        }).then(function(res) {
+            assert.status(res, 200);
+            assert.deepEqual(!!res.body.properties, true);
+            assert.deepEqual(!!res.body.title, true);
+            assert.deepEqual(!!res.body.required, true);
+            assert.deepEqual(!!res.body.type, true);
+        });
     });
 
 });
